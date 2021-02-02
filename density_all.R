6d482343082878ebcb3b589602ff6ccb3ee7d6a0
Sukerfish <viper31jas@aim.com>
2021-02-02 15:08:53 -0500

    works at taxa level

--- density_all.R
+++ density_all.R
@@ -20,14 +20,14 @@ odbcClose(conn)
 # select gear and bay
 RefsList <- MonthlyMaster %>%
   filter(Gear == 20) %>%
-  filter(str_detect(Reference,"^TBM"))
+  filter(str_detect(Reference,"^APM"))
 
 # Establish Zone filter
 ZoneFilter = c("A"
                ,"B"
-               ,"C"
-               ,"D"
-               ,"E"
+               #,"C"
+               #,"D"
+               #,"E"
 )
 
 # clean up biology and merge with selected references
@@ -40,9 +40,9 @@ CleanHRBio <- osg_ComBio(CleanBio, SpeciesList)
 
 #Red Tide Switch
 #Coded as months of interest: e.g., Jul-Oct
-RTS <- c(6:9)
-#RTS <- c(1:3)
-StartYear <- 1998
+#RTS <- c(6:9)
+RTS <- c(1:3)
+StartYear <- 2001
 EndYear <- 2017
 effort <- (140/100)
 
@@ -50,16 +50,13 @@ effort <- (140/100)
 RT_Abund <- CleanHRBio %>%
   mutate(season = "Winter") %>%
   mutate(season = replace(season, month %in% c(4:7), "Spring")) %>%
-  #mutate(CPUE = N2/effort) %>%
   mutate(RTLogic = "Before") %>%
   mutate(RTLogic = replace(RTLogic, year == 2005, "During")) %>%
   mutate(RTLogic = replace(RTLogic, year > 2005, "After")) %>%
-  #filter(month %in% RTS) %>%
+  filter(month %in% RTS) %>%
   filter(#remove early years
     year >= StartYear) %>%
   filter(Zone %in% ZoneFilter)
-
-
 
 # spread via scientific name
 HaulFullTBM <- RT_Abund %>%
@@ -92,28 +89,6 @@ HaulFullCleanTBM <- HaulFullTBM %>%
   inner_join(HaulAbunTBM) %>%
   mutate(CPUE = total/effort)
 
-
-# 
-# # spread via scientific name and CPUE
-# HaulFull <- RT_Abund %>%
-#   spread(Scientificname, CPUE) %>%
-#   replace(is.na(.),0)
-# # set zone as factor for rank abundance stuff
-# HaulFull$Zone    <- as.factor(HaulFull$Zone)
-# HaulFull$RTLogic <- as.factor(HaulFull$RTLogic)
-# HaulFull$RTLogic <- ordered(HaulFull$RTLogic, levels = c("Before", "During", "After"))
-# HaulFull$Stratum <- as.factor(HaulFull$Stratum)
-# 
-# # # pull all environmental data out
-# HaulEnv <- HaulFull %>%
-#   select(Reference:RTLogic) %>%
-#   as.data.frame()
-# 
-# # pull all abundance data out
-# HaulAbun <- HaulFull %>%
-#   subset(select = -c(Reference:RTLogic)) %>%
-#   as.data.frame()
-
 # summary information about the samples
 HaulCount <- HaulFullCleanTBM %>%
   count(year)
@@ -126,7 +101,7 @@ library(goeveg)
 library(scales)
 library(ggrepel)
 
-##### Yearly Resampled Species Richness ####
+##### Yearly Resampled Species density ####
 
 # Resample using the minimum samples in the paired values
 HaulSub <- HaulFullCleanTBM %>%
@@ -135,43 +110,103 @@ HaulSub <- HaulFullCleanTBM %>%
   sample_n(HaulMin, replace = FALSE) %>%
   ungroup()
 
-# calculate average richness per haul using the subsampled data
+HaulSub_Abun <- HaulSub %>%
+  subset(select = -c(Stratum:RTLogic)) %>%
+  subset(select = -c(total:CPUE)) %>%
+  subset(select = -c(Reference:month))
+
+HaulSub_Dens <- HaulSub_Abun %>%
+  mutate(across(!c(year), ~{.x/effort}))
+
+# # initialize
+# testing <- setNames(data.frame(matrix(ncol = ncol(HaulSub_Abun),
+#                                       nrow = 0)),
+#                     c(colnames(HaulSub_Abun)))
+# for (i in 1:((EndYear-StartYear)+1)){
+#   temp_df <- HaulSub_Abun %>%
+#     # filter by year and remove extra columns
+#     filter(year == i+(StartYear-1))
+#     #subset(select = CPUE)
+#   # assign year to row
+#   testing[i,1]  <- i+(StartYear-1)
+#   # calculate richness per haul and average it before placement in matrix
+#   for (j in 2:ncol(HaulSub_Abun)){
+#     testing[i,j] <- mean(temp_df[,j], na.rm = TRUE)
+#   }
+# }
+
+testtt <- HaulSub_Dens %>%
+  group_by(year) %>%
+  summarise(across(everything(), mean)) %>%
+  pivot_longer(!year, names_to = "species", values_to = "CPUE")
+
+
+
+# calculate average density per haul using the subsampled data
 # initialize
-Haul_AvgRich <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), c("year", "avg_richness"))
+Haul_AvgDensity <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), c("year", "avg_density"))
 for (i in 1:((EndYear-StartYear)+1)){
   temp_df <- HaulSub %>%
     # filter by year and remove extra columns
     filter(year == i+(StartYear-1)) %>%
     subset(select = CPUE)
     # assign year to row
-    Haul_AvgRich[i,1]  <- i+(StartYear-1)
+    Haul_AvgDensity[i,1]  <- i+(StartYear-1)
     # calculate richness per haul and average it before placement in matrix
-    Haul_AvgRich[i,2] <- mean(temp_df$CPUE)
+    Haul_AvgDensity[i,2] <- mean(temp_df$CPUE)
   }
 
 # summary stats and plotting processing
-Haul_tsRich <- Haul_AvgRich %>%
+Haul_tsDensity <- Haul_AvgDensity %>%
   # calculate as anomalies
-  mutate(anom.Rich = avg_richness - mean(avg_richness, na.rm = TRUE))
+  mutate(anom.Dens = avg_density - mean(avg_density, na.rm = TRUE))
 
-RichnessAnom_Metrics <- Haul_tsRich %>%
+DensityAnom_Metrics <- Haul_tsDensity %>%
   # calculate mean/CIs
-  summarise(mean.Rich = mean(avg_richness, na.rm = TRUE),
-            sd.anom.Rich = sd(anom.Rich, na.rm = TRUE),
-            n.Rich = n()) %>%
-  mutate(se.anom.Rich = sd.anom.Rich / sqrt(n.Rich),
-         lower.ci.anom.Rich = 0 - (1.96 * se.anom.Rich),
-         upper.ci.anom.Rich = 0 + (1.96 * se.anom.Rich))
+  summarise(mean.Dens = mean(avg_density, na.rm = TRUE),
+            sd.anom.Dens = sd(anom.Dens, na.rm = TRUE),
+            n.Dens = n()) %>%
+  mutate(se.anom.Dens = sd.anom.Dens / sqrt(n.Dens),
+         lower.ci.anom.Dens = 0 - (1.96 * se.anom.Dens),
+         upper.ci.anom.Dens = 0 + (1.96 * se.anom.Dens))
 
 ####### Plots ######
 # Richness over time plot
-ggplot(Haul_tsRich, aes(x=year))+
-  geom_line(aes(y=anom.Rich))+
+ggplot(Haul_tsDensity, aes(x=year))+
+  geom_line(aes(y=anom.Dens))+
   geom_hline(aes(yintercept = 0, color = "darkred"))+
-  geom_ribbon(data=merge(RichnessAnom_Metrics, Haul_tsRich),
-              aes(ymin=lower.ci.anom.Rich, 
-                  ymax=upper.ci.anom.Rich), 
+  geom_ribbon(data=merge(DensityAnom_Metrics, Haul_tsDensity),
+              aes(ymin=lower.ci.anom.Dens, 
+                  ymax=upper.ci.anom.Dens), 
               linetype=2, alpha=0.1, color="purple")+
+  theme_bw()+
+  labs(title = "Animals per 100m^2 Over Time")+
+  theme(axis.title = element_text(face = "bold", size = "12"), 
+        axis.text = element_text(size = "12"),
+        strip.text = element_text(face = "bold", size = "14"))+
+  scale_x_continuous(name = "Year", 
+                     limits = c(StartYear,EndYear),
+                     breaks = seq(StartYear,EndYear, 2))+
+  scale_y_continuous(name = "Average Density Anomaly from Long Term Mean",
+                     limits = c(-200,250))+
+  theme(legend.position="none")
+#facet_grid(Zone ~ .)
+
+
+ggplot(data=testtt,
+       aes(x=year, y=CPUE, color=species)) +
+  theme(legend.position="none") +
+  facet_wrap(as.factor(testtt$species), scales = "free") +
+  geom_line()
+
+
+ggplot(testtt, aes(x=year))+
+  geom_line(aes(y=CPUE))+
+  #geom_hline(aes(yintercept = 0, color = "darkred"))+
+  #geom_ribbon(data=merge(DensityAnom_Metrics, Haul_tsDensity),
+              #aes(ymin=lower.ci.anom.Dens, 
+              #    ymax=upper.ci.anom.Dens), 
+             # linetype=2, alpha=0.1, color="purple")+
   theme_bw()+
   labs(title = "Animals per 100m^2 Over Time")+
   theme(axis.title = element_text(face = "bold", size = "12"), 
