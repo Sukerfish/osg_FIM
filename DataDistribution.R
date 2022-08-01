3bbf2919dd7cbe171afada1d2691c1a5501c1868
Sukerfish <47607889+Sukerfish@users.noreply.github.com>
2022-08-01 08:09:08 -0400

    Setting up for test

--- DataDistribution.R
+++ DataDistribution.R
@@ -73,132 +73,13 @@ ggplot(data=TotalAbundance)+
 #           legend.text=element_text(size=20), #change font size of legend text
 #           legend.title=element_text(size=20)) #change font size of legend title 
 # 
-# TBFull <- SiteXSpeciesFull %>%
-#   filter(system == "TB") %>%
-#   subset(select = -c(system)) %>%
-#   column_to_rownames(var = "seasonYear")
 
-CleanHauls <- TidyBio %>%
-  subset(select = c(Reference, system, season, seasonYear, systemZone, Scientificname, N2)) %>%
-  #filter(season == "summer" | season == "winter")
-  filter(season == "winter")
-  #filter(season == "summer")
-
-SiteXSpeciesFull <- CleanHauls %>%
-  mutate(N2 = N2^.25) %>% #fourth-root transform
-  group_by(Reference) %>%
-  spread(Scientificname,N2) %>%
-  ungroup() %>%
-  subset(select = -c(Reference, season)) %>%
-  replace(is.na(.), 0) %>% #replace all NA values with 0s, i.e. counting as true zero
-  group_by(system, seasonYear, systemZone) %>%
-  summarise(across(everything(), ~ mean(.x, na.rm = TRUE)))
-
-systemLogic <- unique(SiteXSpeciesFull$systemZone)
-
-out <- list()
-
-library(vegan)
-
-for (i in 1:length(systemLogic)){
-  tempDF <- SiteXSpeciesFull %>%
-    filter(systemZone == as.character(systemLogic[i])) %>%
-    subset(select = -c(system, systemZone)) %>%
-    column_to_rownames(var = "seasonYear")
-  
-  distmat <- as.matrix(vegdist(tempDF, method ="bray"))
-  
-  name <- as.character(systemLogic[i])
-  
-  out[[name]] <- distmat[,1]
-}
-
-yearLogic <- SiteXSpeciesFull %>%
-  subset(select = c(system, seasonYear, systemZone)) %>%
-  unique() %>%
-  arrange(system, systemZone)
-  # arrange(factor(system, levels = c("AP", "CK", "CH", "TB")), systemZone)
-
-lessgo <- stack(out) %>%
-  rename(commsim = values) %>%
-  rename(systemZone = ind) %>%
-  cbind(seasonYear = yearLogic$seasonYear) %>%
-  cbind(system = yearLogic$system) %>%
-  filter(commsim > 0) %>%
-  group_by(system, seasonYear) %>%
-  summarise(commsim = mean(commsim)) %>%
-  mutate(season = "winter")
-
-
-CleanHauls <- TidyBio %>%
-  subset(select = c(Reference, system, season, seasonYear, systemZone, Scientificname, N2)) %>%
-  #filter(season == "summer" | season == "winter")
-  #filter(season == "winter")
+stuff <- FullRichness %>%
+  filter(system == "CK") %>%
   filter(season == "summer")
 
-SiteXSpeciesFull <- CleanHauls %>%
-  mutate(N2 = N2^.25) %>% #fourth-root transform
-  group_by(Reference) %>%
-  spread(Scientificname,N2) %>%
-  ungroup() %>%
-  subset(select = -c(Reference, season)) %>%
-  replace(is.na(.), 0) %>% #replace all NA values with 0s, i.e. counting as true zero
-  group_by(system, seasonYear, systemZone) %>%
-  summarise(across(everything(), ~ mean(.x, na.rm = TRUE)))
+library(EnvStats)
 
-# TBFull <- SiteXSpeciesFull %>%
-#   filter(system == "TB") %>%
-#   subset(select = -c(system)) %>%
-#   column_to_rownames(var = "seasonYear")
+gof.list <- gofTest(stuff$n)
+plot(gof.list)
 
-
-systemLogic <- unique(SiteXSpeciesFull$systemZone)
-
-out <- list()
-
-for (i in 1:length(systemLogic)){
-  tempDF <- SiteXSpeciesFull %>%
-    filter(systemZone == as.character(systemLogic[i])) %>%
-    subset(select = -c(system, systemZone)) %>%
-    column_to_rownames(var = "seasonYear")
-  
-  distmat <- as.matrix(vegdist(tempDF, method ="bray"))
-  
-  name <- as.character(systemLogic[i])
-  
-  out[[name]] <- distmat[,1]
-}
-
-yearLogic <- SiteXSpeciesFull %>%
-  subset(select = c(system, seasonYear, systemZone)) %>%
-  unique() %>%
-  arrange(system, systemZone)
-# arrange(factor(system, levels = c("AP", "CK", "CH", "TB")), systemZone)
-
-temp <- stack(out) %>%
-  rename(commsim = values) %>%
-  rename(systemZone = ind) %>%
-  cbind(seasonYear = yearLogic$seasonYear) %>%
-  cbind(system = yearLogic$system) %>%
-  filter(commsim > 0) %>%
-  group_by(system, seasonYear) %>%
-  summarise(commsim = mean(commsim)) %>%
-  mutate(season = "summer")
-
-fullLessgo <- lessgo %>%
-  rbind(temp) %>%
-  arrange(factor(system, levels = c("AP", "CK", "CH", "TB")))
-
-
-
-ggplot(data=fullLessgo,
-       aes(seasonYear, commsim, color = system)) +
-  theme_bw() + 
-  ggtitle("Bray-Curtis community dissimilarity values over time (averaged by zone)") +
-  xlab("Year") +
-  scale_x_continuous(breaks = seq(1998, 2020, 2)) +
-  ylab("Community dissimilarity value") +
-  geom_line() +
-  facet_wrap(as.factor(fullLessgo$season)) +
-  theme(strip.text.x = element_text(size = 20)) +
-  theme(strip.text.y = element_text(size = 20))
