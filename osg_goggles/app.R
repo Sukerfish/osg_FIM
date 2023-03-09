7a57fadf7f27f68a4e06e74d0834aba62d50201a
Sukerfish <viper31jas@aim.com>
2023-03-09 09:22:19 -0500

    remove server-side data processing

--- osg_goggles/app.R
+++ osg_goggles/app.R
@@ -3,114 +3,22 @@ library(tidyverse)
 library(ggplot2)
 library(shinyBS)
 
-load('TidyGearCode20.Rdata')
-
-#### Z scored abundance ####
-
-#grab basic haul data and counts
-CleanHauls <- TidyBio %>%
-  subset(select = c(Reference, system, season, seasonYear, systemZone, Scientificname, N2))
-
-#full site by species matrix
-SiteXSpeciesFull <- CleanHauls %>%
-  pivot_wider(id_cols = Reference:systemZone,
-              names_from = Scientificname,
-              values_from = N2,
-              values_fill = 0) %>% #replace all NA values with 0s, i.e. counting as true zero
-  ungroup()
-
-#collapse full site x species matrix to long term means
-LTxSpeciesMeans <- SiteXSpeciesFull %>%
-  subset(select = -c(Reference, systemZone, seasonYear)) %>%
-  group_by(system, season) %>%
-  summarise(across(everything(), ~ mean(.x, na.rm = TRUE)))
-
-#make matching standard deviation matrix to long term means
-LTxSpeciesStdev <- SiteXSpeciesFull %>%
-  subset(select = -c(Reference, systemZone, seasonYear)) %>%
-  group_by(system, season) %>%
-  summarise(across(everything(), ~ sd(.x, na.rm = TRUE)))
-
-#Z score conversion process
-YearXSpeciesZ <- SiteXSpeciesFull %>%
-  subset(select = -c(Reference, systemZone)) %>%
-  group_by(system, season, seasonYear) %>%
-  summarise(across(everything(), ~ mean(.x, na.rm = TRUE))) %>% #collapse to annual means 
-  ungroup() %>%
-  pivot_longer(cols = !c(system:seasonYear),
-               names_to = "Scientificname",
-               values_to = "avg") %>%
-  #expand back out to long form for leftjoins with LT mean and LT stdev
-  ungroup() %>%
-  left_join(pivot_longer(data = LTxSpeciesMeans, #LT mean column added
-                         cols = !c(system:season),
-                         names_to = "Scientificname",
-                         values_to = "LTmean")) %>%
-  left_join(pivot_longer(data = LTxSpeciesStdev, #LT stdev column added
-                         cols = !c(system:season),
-                         names_to = "Scientificname",
-                         values_to = "stdev")) %>%
-  group_by(system, season, seasonYear) %>%
-  mutate(zscore = ((avg - LTmean)/stdev)) %>% #calculate zscores using annual means
-  ungroup() %>%
-  filter(LTmean > 0) %>% #remove taxa entirely absent from each system - if LTmean = 0, taxa never observed
-  filter(Scientificname != "No fish") %>%
-  mutate(system = factor(system, levels = c("AP", "CK", "TB", "CH")))
+load('ShinyGearCode20.RData')
 
 taxaList = unique(YearXSpeciesZ$Scientificname) %>%
   sort()
-
-#### z scored hydro data #####
-cleanHydro <- SiteXSpeciesFull %>%
-  select(Reference:seasonYear) %>%
-  left_join(HydroList)
-
-#collapse full site x hydro matrix to long term means
-LTxHydroMeans <- cleanHydro %>%
-  subset(select = -c(Reference, seasonYear, Sampling_Date, year, month, Depth)) %>%
-  group_by(system, season) %>%
-  summarise(across(everything(), ~ mean(.x, na.rm = TRUE)))
-
-LTxHydroStdev <- cleanHydro %>%
-  subset(select = -c(Reference, seasonYear, Sampling_Date, year, month, Depth)) %>%
-  group_by(system, season) %>%
-  summarise(across(everything(), ~ sd(.x, na.rm = TRUE)))
-
-#Z score conversion process
-YearXHydroZ <- cleanHydro %>%
-  subset(select = -c(Reference, Sampling_Date, year, month, Depth)) %>%
-  group_by(system, season, seasonYear) %>%
-  summarise(across(everything(), ~ mean(.x, na.rm = TRUE))) %>% #collapse to annual means 
-  ungroup() %>%
-  pivot_longer(cols = !c(system:seasonYear),
-               names_to = "param",
-               values_to = "avg") %>%
-  #expand back out to long form for leftjoins with LT mean and LT stdev
-  ungroup() %>%
-  left_join(pivot_longer(data = LTxHydroMeans, #LT mean column added
-                         cols = !c(system:season),
-                         names_to = "param",
-                         values_to = "LTmean")) %>%
-  left_join(pivot_longer(data = LTxHydroStdev, #LT stdev column added
-                         cols = !c(system:season),
-                         names_to = "param",
-                         values_to = "stdev")) %>%
-  group_by(system, season, seasonYear) %>%
-  mutate(zscore = ((avg - LTmean)/stdev)) %>% #calculate zscores using annual means
-  ungroup() %>%
-  mutate(system = factor(system, levels = c("AP", "CK", "TB", "CH")))
 
 hydroList = unique(YearXHydroZ$param) %>%
   sort()
 
 ######### UI ##########
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
 
     # Application title
     titlePanel("FWRI FIM Data Exploration"),
 
-    # Sidebar with a slider input for number of bins 
+    # control buttoms 
     tabsetPanel(
       tabPanel(title = "Biology",
                actionButton(inputId = "reload", 
@@ -137,7 +45,7 @@ ui <- fluidPage(
         label = "Smooth"
       ),
 wellPanel(
-        # Show a plot of the generated distribution
+        # Show a plot
            plotOutput("taxaPlot")
         )
     ),
@@ -155,14 +63,14 @@ tabPanel(title = "Hydrology",
            label = "Smooth"
          ),
          wellPanel(
-           # Show a plot of the generated distribution
+           # Show a plot
            plotOutput("hydroPlot")
          )
 )
 ))
 
 ######## server ########
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output, session) {
 
   chunk <- eventReactive(input$bio_var, {
