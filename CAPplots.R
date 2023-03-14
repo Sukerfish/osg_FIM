a8259ac2366b7bb97841334ea004854cbdc05049
Sukerfish <47607889+Sukerfish@users.noreply.github.com>
2023-03-13 21:30:42 -0400

    corrected explained variance methods

--- CAPplots.R
+++ CAPplots.R
@@ -11,44 +11,33 @@ library(RColorBrewer)
 library(egg)
 library(patchwork)
 
-#source ('http://www.davidzeleny.net/anadat-r/doku.php/en:customized_functions:ordicenter?do=export_code&codeblock=0')
-ordicenter <- function (ord, groups, display = "sites", w = weights(ord, display), 
-                        show.groups, ...) 
-{
-  weights.default <- function(object, ...) NULL
-  pts <- scores(ord, display = display, ...)
-  w <- eval(w)
-  if (length(w) == 1) 
-    w <- rep(1, nrow(pts))
-  if (is.null(w)) 
-    w <- rep(1, nrow(pts))
-  if (!missing(show.groups)) 
-  {
-    take <- groups %in% show.groups
-    pts <- pts[take, , drop = FALSE]
-    groups <- groups[take]
-    w <- w[take]
-  }
-  out <- seq(along = groups)
-  inds <- names(table(groups))
-  for (is in inds) 
-  {
-    gr <- out[groups == is]
-    if (length(gr) > 1)
-    {
-      X <- pts[gr, ]
-      W <- w[gr]
-      ave <- apply(X, 2, weighted.mean, w = W)
-      vegan:::ordiArgAbsorber(ave[1], ave[2], labels = is, FUN = text, ...)
-    }
-    if (length(gr) == 1)
-    {
-      X <- pts[gr, ]
-      W <- w[gr]
-      vegan:::ordiArgAbsorber(X[1], X[2], labels = is, FUN = text, ...)
-    }
-  }
-  invisible()
+#source https://github.com/jonpeake/forage-fish-public-code/blob/main/ForageFigures_CODE.R
+cap_pctvars <- function(cap_out){
+  # A function that takes the output of the CAPdiscrim function and 
+  # produces the percent of among-group variability explained by each
+  # canonical axis.
+  #
+  # Inputs: 
+  # cap_out = 'CAPDiscrim' function list output
+  # 
+  # Output: 
+  # pct_var = A numerical array with dimensions 1 x p, where p is the
+  #           number of canonical axes, corresponding to the percent
+  #           of among-group variability explained by each axis.
+  #         
+  
+  # Extract eigenvalues from manova sublist
+  eig     <- cap_out[["manova"]][["Eigenvalues"]]
+  
+  # Convert eigenvalues to analog of canonical variability
+  vars    <- eig*cap_out$tot*(cap_out$varm/100)/(eig+1)
+  
+  # Calculate percent of variability explained by each axis
+  pct_var <- t(100*vars/sum(vars))
+  
+  # Trim rows not corresponding to canonical axes
+  pct_var <- pct_var[1:ncol(cap_out$x),]
+  return(pct_var)
 }
 
 load('TidyGearCode20.Rdata')
@@ -129,7 +118,8 @@ plots <- list()
 sppvecs_list <- list()
 for(i in systemSeason_list$systemSeason){
   print(i) #watch progress through list
-  var_CA <- round(100 * CAPsforAll[[i]][["lda.other"]][["svd"]]^2/sum(CAPsforAll[[i]][["lda.other"]][["svd"]]^2), 2)
+  var_CA <- round(cap_pctvars(CAPsforAll[[i]]), 2)
+  #var_CA <- round(100 * CAPsforAll[[i]][["lda.other"]][["svd"]]^2/sum(CAPsforAll[[i]][["lda.other"]][["svd"]]^2), 2)
   site_scores <- data.frame(CAPsforAll[[i]]$x)
   spp_vecs <- data.frame(CAPsforAll[[i]]$cproj)
   sppvecs_list[[i]] <- spp_vecs
@@ -357,3 +347,104 @@ testing <- wrap_plots(plots$a,
                       ncol = 4)
 
 plot(testing)
+
+### centroids ####
+for(i in systemSeason_list$systemSeason){
+df <- SXS_full %>% #filter out systemSeason of interest
+  filter(systemSeason %in% i)
+
+df_spe <- df %>% #pull out taxa only
+  subset(select = -c(systemSeason, seasonYear, Reference, systemZone, BottomVegCover, Temperature)) %>%
+  select(which(!colSums(., na.rm=TRUE) %in% 0))
+
+df_env <- data.frame(df %>% #pull out environmental variables
+                       subset(select = c(systemSeason, seasonYear, BottomVegCover, Temperature)) %>%
+                       mutate(contYear = as.numeric(as.character(seasonYear))))
+
+CAPsforAll[[i]]$centroids <- centroids.long(sites.long(ordiplot(CAPsforAll[[i]])),
+                                            df_env$seasonYear,
+                                            centroids.only = TRUE)
+}
+
+
+##### centroids with sppvec plots #######
+for(i in systemSeason_list$systemSeason){
+  print(i) #watch progress through list
+  var_CA <- round(cap_pctvars(CAPsforAll[[i]]), 2)
+  #var_CA <- round(100 * CAPsforAll[[i]][["lda.other"]][["svd"]]^2/sum(CAPsforAll[[i]][["lda.other"]][["svd"]]^2), 2)
+  site_scores <- data.frame(CAPsforAll[[i]]$x)
+  middles <- data.frame(CAPsforAll[[i]]$centroids)
+  spp_vecs <- data.frame(CAPsforAll[[i]]$cproj) %>%
+                           arrange(desc(abs(LD1))) %>%
+                           slice_head(n = 10)
+  sppvecs_list[[i]] <- spp_vecs
+  mult <- 6
+  plots[[i]] <- ggplot(middles) + 
+    geom_vline(xintercept = 0,
+               colour     = "grey70",
+               size       = .25) +
+    geom_hline(yintercept = 0,
+               colour     = "grey70",
+               size       = .25) +
+    scale_x_continuous(limits       = symmetric_range((1+mult)*spp_vecs$LD1),
+                       breaks       = c(-6,-3,0,3,6),
+                       minor_breaks = NULL) +
+    scale_y_continuous(limits       = symmetric_range,
+                       breaks       = c(-6,-3,0,3,6),
+                       minor_breaks = NULL) +
+    geom_point(aes(x    = axis1c, 
+                   y    = axis2c, 
+                   fill = as.numeric(as.character(df_env.seasonYear))),
+               size   = 4, 
+               stroke = 0.1,
+               pch    = 21, 
+               colour = "black") +
+    labs(title = i,
+         x     = paste("CA1 (",var_CA[1],"%)",sep=""),
+         y     = paste("CA2 (",var_CA[2],"%)",sep=""),
+         fill  = NULL) +
+    scale_fill_gradient(
+      low = "red",
+      high = "blue",
+      space = "Lab",
+      na.value = "grey50",
+      guide = "colourbar",
+      aesthetics = "fill"
+    ) +
+    theme_bw() +
+    theme(legend.text       = element_text(size=rel(0.8)),
+          legend.position   = c(0.055,0.89),
+          legend.background = element_blank(),
+          legend.key        = element_blank(),
+          panel.grid        = element_blank()) +
+    geom_segment(data = spp_vecs,
+                 aes(x    = 0,
+                     xend = mult*LD1,
+                     y    = 0,
+                     yend = mult*LD2),
+                 arrow = arrow(length = unit(0.1,"inches")),
+                 color = "grey1",
+                 size  = .75,
+                 alpha = 0.6)+
+  # geom_label_repel(data = spp_vecs,
+  #                  aes(x     = mult*LD1 + 0.2*cos(atan(LD2/LD1))*sign(LD1),
+  #                      y     = mult*LD2 + 0.2*sin(atan(LD2/LD1))*sign(LD2),
+  #                      label = rownames(spp_vecs)),
+  #                  segment.alpha = 0,
+  #                  size          = 3.25,
+  #                  color         = "blue",
+  #                  fontface      = "bold",
+  #                  fill          = "white",
+  #                  alpha         = 0.7,
+  #                  box.padding   = .25,
+  #                  lineheight    = 0.4,
+  #                  label.size    = 0.25,
+  #                  nudge_x       = ifelse(spp_vecs$LD1>0,0.05,-0.05),
+  #                  nudge_y       = ifelse(spp_vecs$LD2>-0.02,0.05,-0.05),
+  #                  force         = 0.3) +
+  guides(fill = guide_legend(override.aes = list(size = 2.5),
+                             keyheight    = 0.15,
+                             keywidth     = 0.2))
+  
+  #ggsave(paste("./Outputs/CAPs/CAP_", i, ".tiff", sep = ""), CAPsforAll[[i]]$plots, width = 8, height = 8, dpi = 400)
+}
