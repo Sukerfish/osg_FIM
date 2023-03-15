5f145a2950af3245c05898f5e6b0f1be1494d415
Sukerfish <47607889+Sukerfish@users.noreply.github.com>
2023-03-14 21:27:46 -0400

    remove seasonYear from dbrda. Build plots from scratch and patch together as one.

--- RDA.R
+++ RDA.R
@@ -4,6 +4,10 @@
 #### data input ######
 library(tidyverse)
 library(vegan)
+library(egg)
+library(ggplot2)
+library(ggrepel)
+library(patchwork)
 
 load('TidyGearCode20.Rdata')
 
@@ -60,7 +64,7 @@ for(i in systemSeason_list$systemSeason){
   
   bf <- (vegdist(df_spe))^0.5 #Bray-Curtis w/ sqrt to prevent negative eigenvalues
   
-  rda = dbrda(bf ~ seasonYear + Temperature + BottomVegCover,
+  rda = dbrda(bf ~ Temperature + BottomVegCover,
                  data = df_env,
                  #strata = df_env$seasonYear,
                  #add = "lingoes",
@@ -68,18 +72,297 @@ for(i in systemSeason_list$systemSeason){
                  #method="bray", 
                  #permutations = 999,
   )
-  # 
-  # detrend_Pred_mod <- rda
-  # 
-  # detrend_scores        <- detrend_Pred_mod$CA$u %*% diag(sqrt(detrend_Pred_mod$CA$eig))
-  # detrend_pctVar        <- detrend_Pred_mod$CA$eig/sum(detrend_Pred_mod$CA$eig)
-  # detrend_cumsum        <- cumsum(detrend_pctVar)
-  # detrend_scoreRetained <- data.frame(detrend_scores[, 1:26])
-  #   
-  RDAsforAll[[i]] <- rda
+  env <- envfit(rda, df_env[,3:4])
+  scores <- scores(rda, display = "all")
+  RDAsforAll[[i]]$rda <- rda
+  RDAsforAll[[i]]$env <- env
+  RDAsforAll[[i]]$scores <- scores
 }
 
 #save(RDAsforAll, file = "RDAsforAll.RData")
 load('RDAsforAll.Rdata')
 
-scores_sites <- scores(RDAsforAll[[i]], display = "wa")
+plotsforAll <- list()
+#rdaVecslist <- list()
+for(i in systemSeason_list$systemSeason){
+ print(i)
+  mult <- 1
+  mult_vec <- 1
+  scores_sites <- data.frame(RDAsforAll[[i]]$scores$sites)
+  vecs <- data.frame(RDAsforAll[[i]]$env$vectors$arrows)
+  #rdaVecslist[[i]] <- vecs
+  middles <- data.frame(RDAsforAll[[i]]$scores$centroids)
+  plotSub <- SXS_full %>% #filter out systemSeason of interest just as in rda loop (df_env)
+    filter(systemSeason %in% i) %>%
+    select(seasonYear) %>%
+    cbind(scores_sites) %>%
+    group_by(seasonYear) %>%
+    summarise(across(everything(), ~ mean(.x, na.rm = TRUE)))
+  # grps <- data.frame(rownames(middles) %>%
+  #   str_remove_all("seasonYear")) %>%
+  #   rename(seasonYear = rownames.middles......str_remove_all..seasonYear..)
+  
+  plotsforAll[[i]] <- ggplot(plotSub) +
+    geom_vline(xintercept = 0,
+               colour     = "grey70",
+               size       = 1) +
+    geom_hline(yintercept = 0,
+               colour     = "grey70",
+               size       = 1) +
+    scale_x_continuous(limits       = symmetric_range,
+                       breaks       = c(-6,-1,0,1,6),
+                       minor_breaks = NULL) +
+    scale_y_continuous(limits       = symmetric_range,
+                       breaks       = c(-6,-1,0,1,6),
+                       minor_breaks = NULL) +
+    labs(title = i, 
+         x     = paste("CA1 (", round(100*RDAsforAll[[i]]$rda$CCA$eig[1]/RDAsforAll[[i]]$rda$tot.chi,2), "%)", sep = ""),
+         y     = paste("CA1 (", round(100*RDAsforAll[[i]]$rda$CCA$eig[2]/RDAsforAll[[i]]$rda$tot.chi,2), "%)", sep = ""))+
+    scale_color_manual(values = c("blue","darkgreen"),
+                       guide  = guide_none()) +
+    # geom_point(data = scores_sites,
+    #            aes(x = mult*dbRDA1,
+    #                y = mult*dbRDA2,
+    #            ),
+    #            size   = 1,
+    #            stroke = 0.1,
+    #            pch    = 21,
+    #            colour = "black") +
+    geom_point(aes(x = mult*dbRDA1,
+                   y = mult*dbRDA2,
+                   fill = as.numeric(as.character(seasonYear))),
+               size   = 4,
+               stroke = 0.1,
+               pch    = 21,
+               colour = "black") +
+    scale_fill_gradient(
+      low = "red",
+      high = "blue",
+      space = "Lab",
+      na.value = "grey50",
+      guide = "colourbar",
+      aesthetics = "fill"
+    ) +
+    theme_bw() +
+    theme(legend.text       = element_text(size=rel(0.8)),
+          legend.position   = c(0.055,0.89),
+          legend.background = element_blank(),
+          legend.key        = element_blank(),
+          panel.grid        = element_blank()) +
+    geom_segment(data = vecs,
+                 aes(x     = 0, 
+                     xend  = mult_vec*dbRDA1, 
+                     y     = 0, 
+                     yend  = mult_vec*dbRDA2, 
+                     #color = "blue"
+                       ),
+                 arrow = arrow(length = unit(.1,"inches")),
+                 size  = 1,
+                 alpha = 0.6) +
+    geom_label_repel(data = vecs,
+                     aes(x      = mult_vec*dbRDA1,
+                         y      = mult_vec*dbRDA2,
+                         label  = rownames(vecs),
+                         #colour = "blue",
+                         #size   = "blue"
+                           ),
+                     force         = 0.1,
+                     alpha         = 0.8,
+                     fontface      = "bold",
+                     label.padding = 0.25,
+                     box.padding   = 0.1,
+                     fill          ="white",
+                     label.r       = 0.25,
+                     label.size    = 0.25,
+                     segment.alpha =0,
+                     nudge_x       = ifelse(vecs$dbRDA1>0,0.1,-0.1),
+                     nudge_y       = ifelse(vecs$dbRDA2>0,0.2,-0.2)) +
+    scale_size_manual(values = c(2.5,3.5),
+                      guide  = guide_none()) +
+    guides(fill = guide_legend(override.aes = list(size = 2.5),
+                               keyheight    = 0.15,
+                               keywidth     = 0.2,
+                               title = "Year"))
+  
+}
+
+fullPlot <- wrap_plots(plotsforAll,
+                      ncol = 4)
+
+plot(fullPlot)
+
+
+######### BRUTE FORCE PLOTTING ########
+# plots$a <- plots$AP_winter +
+#   geom_label_repel(data = rdaVecslist$AP_winter,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$AP_winter),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$AP_winter$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$AP_winter$dbRDA2>0,0.2,-0.2))
+# 
+# plots$b <- plots$AP_summer +
+#   geom_label_repel(data = rdaVecslist$AP_summer,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$AP_summer),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$AP_summer$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$AP_summer$dbRDA2>0,0.2,-0.2))
+# 
+# plots$c <- plots$CK_winter +
+#   geom_label_repel(data = rdaVecslist$CK_winter,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$CK_winter),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$CK_winter$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$CK_winter$dbRDA2>0,0.2,-0.2))
+# 
+# plots$d <- plots$CK_summer +
+#   geom_label_repel(data = rdaVecslist$CK_summer,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$CK_summer),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$CK_summer$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$CK_summer$dbRDA2>0,0.2,-0.2))
+# 
+# plots$e <- plots$TB_winter +
+#   geom_label_repel(data = rdaVecslist$TB_winter,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$TB_winter),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$TB_winter$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$TB_winter$dbRDA2>0,0.2,-0.2))
+# 
+# plots$f <- plots$TB_summer +
+#   geom_label_repel(data = rdaVecslist$TB_summer,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$TB_summer),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$TB_summer$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$TB_summer$dbRDA2>0,0.2,-0.2))
+# 
+# plots$g <- plots$CH_winter +
+#   geom_label_repel(data = rdaVecslist$CH_winter,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$CH_winter),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$CH_winter$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$CH_winter$dbRDA2>0,0.2,-0.2))
+# 
+# plots$h <- plots$CH_summer +
+#   geom_label_repel(data = rdaVecslist$CH_summer,
+#                    aes(x      = mult*dbRDA1,
+#                        y      = mult*dbRDA2,
+#                        label  = rownames(rdaVecslist$CH_summer),
+#                        #colour = "blue",
+#                        #size   = "blue"
+#                    ),
+#                    force         = 0.1,
+#                    alpha         = 0.8,
+#                    fontface      = "bold",
+#                    label.padding = 0.25,
+#                    box.padding   = 0.1,
+#                    fill          ="white",
+#                    label.r       = 0.25,
+#                    label.size    = 0.25,
+#                    segment.alpha =0,
+#                    nudge_x       = ifelse(rdaVecslist$CH_summer$dbRDA1>0,0.1,-0.1),
+#                    nudge_y       = ifelse(rdaVecslist$CH_summer$dbRDA2>0,0.2,-0.2))
+# 
+# 
+# 
+# testing <- wrap_plots(plots$a,
+#                       plots$c,
+#                       plots$e,
+#                       plots$g,
+#                       plots$b,
+#                       plots$d,
+#                       plots$f,
+#                       plots$h,
+#                       ncol = 4)
+# 
+# plot(testing)
