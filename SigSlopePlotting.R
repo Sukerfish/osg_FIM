34f79582bcc9b6385fe423a2b48e5c45ca83d455
Sukerfish <47607889+Sukerfish@users.noreply.github.com>
2023-09-24 20:24:57 -0400

    add ecological group plots

--- SigSlopePlotting.R
+++ SigSlopePlotting.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(viridis)
 library(ggallin)
 library(egg)
+library(patchwork)
 
 osg_theme <- readRDS('osg_theme.rds')
 
@@ -16,6 +17,8 @@ for (i in unique(systems)){
   sigSlopes[[i]] <- read_excel("Outputs/SigSlopes.xlsx",
                           sheet = i)
 }
+
+ecoGroup <- read.csv("Outputs/taxaList_grouped.csv")
 
 sigSlopesdf <- bind_rows(sigSlopes)
 
@@ -71,7 +74,7 @@ YearXSpeciesZ <- SiteXSpeciesFull %>%
   ungroup() %>%
   mutate(system = factor(system, levels = c("AP", "CK", "TB", "CH"))) %>%
   filter(LTmean > 0) %>% #remove taxa entirely absent from each system - if LTmean = 0, taxa never observed
-  filter(Scientificname != "No fish") 
+  filter(Scientificname != "No fish")
 # #filter(Scientificname == "Leiostomus xanthurus") %>%
 # filter(system == "AP") %>%
 # filter(season == "winter")
@@ -80,12 +83,29 @@ YearXSigSpeciesZ <- sigSlopesdf %>%
   left_join(YearXSpeciesZ) %>%
   mutate(system = factor(system, levels = c("AP", "CK", "TB", "CH"))) %>%
   group_by(system, season) %>%
+  left_join(ecoGroup) %>%
+  mutate(group = as.factor(group)) %>%
   #arrange(desc(abs(slope)), .by_group = TRUE) %>%
   #ungroup() %>%
   #group_by(Scientificname) %>%
   #slice_head(n = 5) %>%
-  #filter(sign(slope) == -1) %>%
+  #filter(sign(slope) == 1) %>%
   mutate(season = str_to_title(season))
+
+ecoSigNeg <- YearXSigSpeciesZ %>%
+  filter(sign(slope) == -1) %>%
+#  left_join(ecoGroup) %>%
+  group_by(system, season, seasonYear, group) %>%
+  summarise(meanZ = mean(zscore))
+
+ecoSigPos <- YearXSigSpeciesZ %>%
+  filter(sign(slope) == 1) %>%
+  #left_join(ecoGroup) %>%
+  group_by(system, season, seasonYear, group) %>%
+  summarise(meanZ = mean(zscore))
+
+groupList <- unique(ecoGroup$group)
+names(groupList) <- unique(ecoGroup$group)
 
 system_name <- c(
   AP = "Apalachicola Bay",
@@ -94,7 +114,7 @@ system_name <- c(
   CH = "Charlotte Harbor"
 )
 
-sigSlopePlot <- ggplot(data = YearXSigSpeciesZ) +
+negsigSlopePlot <- ggplot(data = YearXSigSpeciesZ) +
   geom_line(aes(x = seasonYear,
                 y = zscore,
                 color = Scientificname),
@@ -105,8 +125,50 @@ sigSlopePlot <- ggplot(data = YearXSigSpeciesZ) +
   scale_x_continuous(breaks = seq(1998,2020,4)) +
   osg_theme +
   theme(legend.position = "none")
+
+negecoSlopePlot <- ggplot(data = ecoSigNeg) +
+  geom_line(aes(x = seasonYear,
+                y = meanZ,
+                color = group),
+            linewidth = 0.7
+  ) +
+  facet_grid(season~system,
+             labeller = labeller(system = system_name)) +
+  scale_x_continuous(breaks = seq(1998,2020,4)) +
+  scale_color_manual( #need to force legends to be identical (i.e. show all possibilities) to merge w/ patchwork
+    breaks = groupList,
+    values = c("red", "blue", "green3", "black", "orange", "purple", "darkblue", "yellow", "gray"), #use scale shape identities
+    drop = FALSE
+  ) +
+  labs(title = "Negative slopes") +
+  theme_bw()
+
+posecoSlopePlot <- ggplot(data = ecoSigPos) +
+  geom_line(aes(x = seasonYear,
+                y = meanZ,
+                color = group),
+            linewidth = 0.7
+  ) +
+  facet_grid(season~system,
+             labeller = labeller(system = system_name)) +
+  scale_x_continuous(breaks = seq(1998,2020,4)) +
+  scale_color_manual( #need to force legends to be identical (i.e. show all possibilities) to merge w/ patchwork
+    breaks = groupList,
+    values = c("red", "blue", "green3", "black", "orange", "purple", "darkblue", "yellow", "gray"), #use scale shape identities
+    drop = FALSE
+  ) +
+  labs(title = "Positive slopes") +
+  theme_bw()
   
-  # scale_color_cmocean(discrete = TRUE,
+ecoSlopes <- wrap_plots(negecoSlopePlot, posecoSlopePlot,
+                        guides = "collect")
+
+# ggsave(plot = ecoSlopes,
+#        filename = "./Outputs/ecoSlopes.png",
+#        width = 16,
+#        height = 9)
+
+# scale_color_cmocean(discrete = TRUE,
   #                     name = "solar") +
   # scale_color_viridis_d(option = "viridis") +
   # scale_color_brewer(palette = "Set1") +
